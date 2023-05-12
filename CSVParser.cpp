c2d51f5b81cc43d1445575864db865545e7570a9
NathanTPerkins <108106121+NathanTPerkins@users.noreply.github.com>
2023-05-12 13:58:24 -0500

    fixed arduino problems

--- CSVParser.cpp
+++ CSVParser.cpp
@@ -27,7 +27,7 @@ csv_parser::parser::parser(const char * filename, int precision = 10){
     this->size = getNumEntries(csv);
     this->data = new char**[this->size];
     for(int i = 0; i < this->size; ++i){
-        this->data[i] = new char*[col_count];0
+        this->data[i] = new char*[col_count];
     }
     for(int i = 0; i < this->size; ++i){
         for(int j = 0; j < col_count; ++j){
